3c2082241147f6ed3b3291838e3ed8337e83e841
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-27 11:24:23 -0700

    Part B: Updating Profile Info

--- profile/profile.js
+++ profile/profile.js
@@ -2,9 +2,9 @@ import '../auth/user.js';
 // > Part A: import updateProfile from fetch-utils.js
 import { updateProfile } from '../fetch-utils.js';
 // > Part B: import getUser and getProfile from fetch-utils.js
-
+import { getUser, getProfile } from '../fetch-utils.js';
 // > Part B: get the user
-const user = null; // ???
+const user = getUser(); // ???
 
 const errorDisplay = document.getElementById('error-display');
 const profileForm = document.getElementById('profile-form');
@@ -18,7 +18,10 @@ let error = null;
 window.addEventListener('load', async () => {
     // > Part B:
     //      - get the profile based on user.id
+    const response = await getProfile(user.id);
     //      - set profile and error state from response object
+    error = response.error;
+    profile = response.data;
 
     if (error) {
         displayError();
@@ -51,7 +54,7 @@ profileForm.addEventListener('submit', async (e) => {
         bio: formData.get('bio'),
     };
     //      - call updateProfile passing in profile update object, capture the response
-    const response = null; // ??????
+    const response = await updateProfile(profileUpdate); // ??????
 
     error = response.error;
 
@@ -71,6 +74,14 @@ profileForm.addEventListener('submit', async (e) => {
 
 function displayProfile() {
     // > Part B: update user name and bio from profile object
+    if (profile) {
+        userNameInput.value = profile.user_name;
+        bioTextArea.value = profile.bio;
+
+        /*if (profile.avatar_url) {
+            preview.src = profile.avatar_url;
+        } */
+    }
 }
 
 function displayError() {
